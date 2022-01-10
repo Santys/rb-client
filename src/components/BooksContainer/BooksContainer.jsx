a243e507e73d07f1645e23ed9de0930aafa48432
Santi Jimenez <jimenez.santi@gmail.com>
2022-01-10 13:24:32 +0100

    Add basic book container

---
+++ src/components/BooksContainer/BooksContainer.jsx
@@ -0,0 +1,19 @@
+import Card1 from '../Card1/Card1';
+
+const BooksContainer = ({ books }) => {
+  return (
+    <div className="container">
+      <div className="row">
+        {books.map((book) => {
+          return (
+            <div className="col-4">
+              <Card1 book={book} />;
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default BooksContainer;
