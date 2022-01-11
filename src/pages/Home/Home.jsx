c538538cf75d180d90424748afc7ab6421619ec9
Santi Jimenez <jimenez.santi@gmail.com>
2022-01-11 21:30:07 +0100

    Create home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,17 @@
+import BooksContainer from '../../components/BooksContainer/BooksContainer';
+import Card1 from '../../components/Card1/Card1';
 import Searchbar from '../../components/Searchbar/Searchbar';
+import bookExample from '../../book_example.json';
+import booksExample from '../../books_example.json';
 
 const Home = () => {
-  return <Searchbar />;
+  return (
+    <>
+      <Searchbar />
+      <BooksContainer books={booksExample} />
+      <Card1 book={bookExample} />
+    </>
+  );
 };
 
 export default Home;
