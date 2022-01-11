a94ac488b673aa2af84f671a6dd9b4af3973899a
Santi Jimenez <jimenez.santi@gmail.com>
2022-01-11 21:30:38 +0100

    Style App and check components

--- src/App.js
+++ src/App.js
@@ -2,27 +2,25 @@ import './App.css';
 import Card1 from './components/Card1/Card1';
 import BooksContainer from './components/BooksContainer/BooksContainer';
 
-import bookExample from './book_example.json';
-import booksExample from './books_example.json';
 // import Searchbar from './components/Searchbar/Searchbar';
 import { Container } from 'react-bootstrap';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import AuthForm from './components/Form/Form';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-grey">
       <Navbar />
       <Container>
-        <h1>Hola!</h1>
-        <h1>Hello</h1>
+        <h1></h1>
         {/* <Searchbar /> */}
+        <AuthForm action={'Log in'} />
+        <h1></h1>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-        <Card1 book={bookExample} />
-        <BooksContainer books={booksExample} />
       </Container>
     </div>
   );
