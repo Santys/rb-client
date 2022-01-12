84154415e0289d1d7edb5b8bdc5bcfa1a392d682
Santi Jimenez <jimenez.santi@gmail.com>
2022-01-12 17:23:36 +0100

    Changes in app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import './App.css';
-import Card1 from './components/Card1/Card1';
-import BooksContainer from './components/BooksContainer/BooksContainer';
 
 // import Searchbar from './components/Searchbar/Searchbar';
 import { Container } from 'react-bootstrap';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Navbar from './components/Navbar/Navbar';
-import AuthForm from './components/Form/Form';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
 
 function App() {
   return (
     <div className="App bg-grey">
       <Navbar />
-      <Container>
-        <h1></h1>
-        {/* <Searchbar /> */}
-        <AuthForm action={'Log in'} />
-        <h1></h1>
+      <Container className="bg-grey">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Container>
     </div>
