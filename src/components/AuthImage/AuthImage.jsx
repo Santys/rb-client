2b90771ec61046fbbe65d6af0f1a4e3517030005
Santi Jimenez <jimenez.santi@gmail.com>
2022-01-12 10:26:25 +0100

    Add authimage component

---
+++ src/components/AuthImage/AuthImage.jsx
@@ -0,0 +1,21 @@
+import { Carousel, Image } from 'react-bootstrap';
+import './AuthImage.css';
+
+const AuthImage = () => {
+  return (
+    <>
+      <div className="maintxt">
+        <Image
+          fluid={true}
+          rounded={true}
+          src="https://images.unsplash.com/photo-1432821596592-e2c18b78144f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        />
+        <span class="overlay-text">
+          <i className="bi bi-book text-light icon-size"></i>
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default AuthImage;
