ca933c08cf7a7998067aecd5673b5720137452f5
Santi Jimenez <jimenez.santi@gmail.com>
2022-01-12 17:22:49 +0100

    Style homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,15 @@
 import BooksContainer from '../../components/BooksContainer/BooksContainer';
-import Card1 from '../../components/Card1/Card1';
 import Searchbar from '../../components/Searchbar/Searchbar';
-import bookExample from '../../book_example.json';
 import booksExample from '../../books_example.json';
 
 const Home = () => {
   return (
-    <>
+    <div className="mt-5 pb-5">
       <Searchbar />
+      <p className="h1 text-dark-green">Top 10 books</p>
+      <hr />
       <BooksContainer books={booksExample} />
-      <Card1 book={bookExample} />
-    </>
+    </div>
   );
 };
 
