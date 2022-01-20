8b76013679cc33737fcdab88d7ca5b32a1206285
Santi Jimenez <jimenez.santi@gmail.com>
2022-01-20 16:53:17 +0100

    Auth context

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProviderWrapper } from './context/auth.context';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProviderWrapper>
+        <App />
+      </AuthProviderWrapper>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
