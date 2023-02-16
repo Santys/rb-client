4c2d02b2cf49398ccfcd4ba3d78db980260e3d7c
Fernando Cases <nandocasesgarcia@gmail.com>
2023-02-16 11:46:29 +0100

    refactor: add accessibility, input validation, add password visibility and conditional rendering

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
 const AuthForm = ({ action, handleAuth }) => {
@@ -6,6 +6,8 @@ const AuthForm = ({ action, handleAuth }) => {
     username: '',
     password: '',
   });
+  const [showPassword, setShowPassword] = useState(false);
+  const [formError, setFormError] = useState('');
 
   const { username, password } = authForm;
 
@@ -16,6 +18,10 @@ const AuthForm = ({ action, handleAuth }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (username.trim() === '' || password.trim() === '') {
+      setFormError('Please enter a valid username and password.');
+      return;
+    }
     handleAuth(username, password);
   };
 
@@ -28,17 +34,23 @@ const AuthForm = ({ action, handleAuth }) => {
           value={username}
           onChange={handleInputChange}
           placeholder="Username"
+          aria-label="Username"
+          required
         />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Control
-          type="password"
+          type={showPassword ? 'text' : 'password'}
           name="password"
           value={password}
           onChange={handleInputChange}
           placeholder="Password"
+          aria-label="Password"
+          required
         />
+        <Form.Text className="text-danger">{formError}</Form.Text>
+        <Form.Check type="checkbox" label="Show password" onChange={() => setShowPassword(!showPassword)} />
       </Form.Group>
       <div className="d-grid gap-2">
         <Button type="submit" className="button-sand">
